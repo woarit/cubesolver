28c4e488e9a220dd1f9a176635f4b518596518aa
Anders Kielsholm <anders@anigif.dk>
2015-12-06 02:06:24 +0100

    Added axis enum class

---
+++ include/cubesolver/axis.h
@@ -0,0 +1,22 @@
+#ifndef CUBESOLVER_AXIS_H
+#define CUBESOLVER_AXIS_H
+
+namespace cube_solver
+{
+
+/**
+ * Representing the axises of the cube.
+ * X: Left and right side
+ * Y: Top and bottom side
+ * Z: Front and back side
+ */
+enum class Axis
+{
+    X,
+    Y,
+    Z
+};
+
+}
+
+#endif //CUBESOLVER_AXIS_H
